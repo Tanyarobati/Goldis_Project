46bc9d508bf91d0eb25810494ea1d0cfea592b44
Tanya Robati <tanyarobati@gmail.com>
2024-12-21 12:36:18 +0330

    fix the cols and rows

--- quasar-project/src/pages/IndexPage.vue
+++ quasar-project/src/pages/IndexPage.vue
@@ -2,13 +2,14 @@
   <q-page>
     <div class="q-pa-md q-pb-xl">
 
+
       <q-carousel
         animated
         v-model="slide"
         arrows
         navigation
         infinite
-        class="custom-carousel"
+        class="custom-carousel "
       >
         <q-carousel-slide
           v-for="(image, index) in carouselImages"
@@ -19,33 +20,36 @@
       </q-carousel>
 
 
-      <div class="q-pa-md">
+      <div class="q-pa-md q-pt-xl">
         <q-row
           class="q-gutter-md justify-evenly"
           :cols="4"
+          style="display: flex; flex-wrap: wrap;"
         >
 
         <q-col
             v-for="product in products"
             :key="product.id"
-            cols="12" sm="6" md="3" lg="3"
+            :cols="12" sm="6" md="3" lg="3"
+            class="product-col"
           >
             <q-card class="my-card">
-
               <q-img
-                :src="product.image"
-                ratio="4:3"
+                :src="product.image || 'https://via.placeholder.com/250x250.png?text=No+Image'"
                 alt="Product Image"
                 class="q-mb-md"
+                :lazy="true"
+                style="max-width: 100%; max-height: 200px; object-fit: cover;"
               />
+
 
               <q-card-section>
                 <div class="text-h6">{{ product.name }}</div>
                 <div class="text-subtitle2 text-grey">{{ product.sold }}</div>
               </q-card-section>
 
-              <q-card-section  align="right">
-                <div >
+              <q-card-section align="right">
+                <div>
                   <span class="text-bold">قیمت:</span> ${{ product.price }}
                 </div>
                 <q-rating
@@ -85,14 +89,20 @@ fetchProducts()
 
 <style>
 .custom-carousel {
-  width: full;
+  width: 100%;
   height: 800px;
-  
+  border-radius: 10px;
 }
 
 .my-card {
   max-width: 250px;
   min-width: 200px;
   box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+  border-radius: 10px;
+}
+
+.product-col {
+  display: flex;
+  justify-content: center;
 }
 </style>
